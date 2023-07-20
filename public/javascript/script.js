b336f3d37059a25eb7a53315ccae23537d03ce07
it-Vega <ituichiev127@gmail.com>
2023-07-20 09:49:42 +0500

    First test commit

---
+++ public/javascript/script.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById("menu");
+function toogle() {
+  menu.classList.toggle("show");
+}
+function hide() {
+  menu.classList.remove("show");
+}
+
+const search = document.getElementById("search_form");
+function change() {
+  search.classList.add("show_form");
+}
+console.log(4);
